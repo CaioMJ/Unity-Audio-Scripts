285caf98892d89624262506ca0c4922d0b7c965e
caiojmini <caiojmini@gmail.com>
2020-11-29 23:19:26 -0300

    Initial commit

---
+++ AudioUtility.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioUtility
+{
+	public static IEnumerator FadeInAudioSource(AudioSource audioSource, float duration, float targetVolume)
+	{
+		audioSource.volume = 0;
+		float currentTime = 0;
+
+		while (currentTime < duration)
+		{
+			currentTime += Time.deltaTime;
+			audioSource.volume = Mathf.Lerp(0, targetVolume, currentTime / duration);
+
+			yield return null;
+		}
+		yield break;
+	}
+
+	public static IEnumerator FadeOutAudioSource(AudioSource audioSource, float duration)
+	{
+		float currentTime = 0;
+		float startVolume = audioSource.volume;
+
+		while (currentTime < duration)
+		{
+			currentTime += Time.deltaTime;
+			audioSource.volume = Mathf.Lerp(startVolume, 0, currentTime / duration);
+
+			yield return null;
+		}
+		yield break;
+	}
+
+	public static IEnumerator CrossfadeAudioSources(AudioSource audioSourceIn, float targetVolumeIn, AudioSource audioSourceOut, float duration)
+	{
+		float currentTime = 0;
+		float startVolumeOut = audioSourceOut.volume;
+		audioSourceIn.volume = 0;
+
+		while (currentTime < duration)
+		{
+			currentTime += Time.deltaTime;
+			audioSourceOut.volume = Mathf.Lerp(startVolumeOut, 0, currentTime / duration);
+			audioSourceIn.volume = Mathf.Lerp(0, targetVolumeIn, currentTime / duration);
+
+			yield return null;
+		}
+		yield break;
+	}
+
+	public static void PlayOneShotWithRandomization(AudioSource audioSource, AudioClip audioClip,
+		float minVolume, float maxVolume, float minPitch, float maxPitch)
+    {
+		float volume = Random.Range(minVolume, maxVolume);
+		audioSource.pitch = Random.Range(minPitch, maxPitch);
+		audioSource.PlayOneShot(audioClip, volume);
+    }
+
+	public static void PlayOneShotWithRandomization(AudioSource audioSource, AudioClip[] audioClipArray,
+	float minVolume, float maxVolume, float minPitch, float maxPitch)
+	{
+		float volume = Random.Range(minVolume, maxVolume);
+		audioSource.pitch = Random.Range(minPitch, maxPitch);
+		audioSource.PlayOneShot(audioClipArray[Random.Range(0, audioClipArray.Length)], volume);
+	}
+}
---
+++ ConcatenateAudioClips.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcatenateAudioClips : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private AudioSource[] audioSource;
+    private int randomIndex;
+    private int lastRandomIndex;
+    private int toggle;
+    private double clipLength;
+    private double nextStartTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clipLength = (double)audioClips[0].samples / audioClips[0].frequency;
+        nextStartTime = AudioSettings.dspTime + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(AudioSettings.dspTime > nextStartTime - 1)
+        {
+            ConcatenateClipsRandomly();
+        }
+    }
+
+    private void ConcatenateClipsRandomly()
+    {
+        toggle = 1 - toggle;
+
+        while (randomIndex == lastRandomIndex)
+        {
+            randomIndex = Random.Range(0, audioClips.Length);
+        }
+        lastRandomIndex = randomIndex;
+
+        audioSource[toggle].clip = audioClips[randomIndex];
+        audioSource[toggle].PlayScheduled(nextStartTime);
+
+        nextStartTime += clipLength;
+        print(audioClips[randomIndex]);
+    }
+}
