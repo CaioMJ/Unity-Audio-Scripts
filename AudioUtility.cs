7dbc06cd9905b9c123104c40eb86d17e0822e9c9
caiojmini <67804071+CaioMJ@users.noreply.github.com>
2021-05-11 11:41:44 -0300

    Update AudioUtility.cs

--- AudioUtility.cs
+++ AudioUtility.cs
@@ -1,9 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public static class AudioUtility
 {
+	
+	public static IEnumerator FadeMixerGroup(AudioMixer audioMixer, string exposedParam, float duration, float targetVolume)
+	{
+		float currentTime = 0;
+		float currentVol;
+
+		audioMixer.GetFloat(exposedParam, out currentVol);
+		currentVol = Mathf.Pow(10, currentVol / 20);
+		float targetValue = Mathf.Clamp(targetVolume, 0.0001f, 1);
+
+		while (currentTime < duration)
+		{
+			currentTime += Time.deltaTime;
+			float newVol = Mathf.Lerp(currentVol, targetValue, currentTime / duration);
+			audioMixer.SetFloat(exposedParam, Mathf.Log10(newVol) * 20);
+			yield return null;
+		}
+		yield break;
+	}
+	
 	public static IEnumerator PlayAndFadeInAudioSource(AudioSource audioSource, float duration, float targetVolume)
 	{
 		audioSource.volume = 0;
